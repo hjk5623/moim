b61e084e3f490773b21cb166a26631a7773e79ae
mee4103 <mee4103@gmail.com>
2019-05-15 17:57:39 +0900

    05월15일 17:56 박보미 - 카테고리 추가

--- moim/clubing/ing_list.php
+++ moim/clubing/ing_list.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
   <head>
@@ -21,6 +22,36 @@
       </ul>
     </nav>
     <section class="sec1"><section>
-    <section class="sec2"><section>
+    <section class="sec2">
+      <ul>
+        <li><a href="shopmain.php">전체</a></li>
+        <li><a href="shopmain.php?mode=clothe">글쓰기</a></li>
+        <li><a href="shopmain.php?mode=cosmetics">요리</a></li>
+        <li><a href="shopmain.php?mode=acc">영화</a></li>
+        <li><a href="shopmain.php?mode=travel">미술</a></li>
+      </ul>
+      <?php
+      if(!empty($mode)&&isset($mode)){
+        $sql = "select * from club where club_category='$mode' order by hit desc";
+      }else{
+        $sql = "select * from club order by hit desc";
+      }
+          $result = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+          for($i=1;$i<13;$i++){ //신상품 12개 정렬
+            $row= mysqli_fetch_array($result);
+            $club_name=$row['club_name'];
+            $club_image=$row['club_image'];
+    ?>
+          	<a href="./list.php">
+              <div>
+          			<div class=""><img src="../img/<?=$image?>" width="350px" height="400px"></div>
+          			<div class=""><b><?=club_name?></b></div><br>
+              </div>
+            </a>
+      <?php
+          }
+      ?>
+    <section>
   </body>
 </html>
